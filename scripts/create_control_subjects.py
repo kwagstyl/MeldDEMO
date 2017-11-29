4f06bee3b7ab6587a5bf55ed678a9be725957689
Sophie <sophie@mc244-125.ich.ucl.ac.uk>
2017-11-29 16:42:47 +0000

    create controls

--- scripts/create_control_subjects.py
+++ scripts/create_control_subjects.py
@@ -1,9 +1,11 @@
 # create control subjects to carry out z-scores
+#import relevant packages
 import numpy as np
 import nibabel as nb
 import argparse
 
-#subjects dir
+
+#parse commandline arguments pointing to subject_dir etc
 parser = argparse.ArgumentParser(description='create mu and std overlays from a set of subjects to carry out z-scoring.')
 parser.add_argument('subject_dir', type=str, 
                     help='path to subject dir')
@@ -13,7 +15,7 @@ parser.add_argument('subject_ids',
 
 args = parser.parse_args()
 
-#list of subjects
+#save subjects dir and subject ids. import the text file containing subject ids
 subject_dir=args.subject_dir
 subject_ids_filename=args.subject_ids
 subject_ids=np.loadtxt(subject_dir+subject_ids_filename, dtype='str')
@@ -24,9 +26,15 @@ def import_mgh(filename):
     mmap_data=mgh_file.get_data()
     array_data=np.ndarray.flatten(mmap_data)
     return array_data;
-    
-    
-measures=np.array(['.thickness_z_on_lh.sm10.mgh', '.asym.thickness_z.sm10.mgh',
+
+def save_mgh(filename,array, demo):
+   """ save mgh file using nibabel and imported demo mgh file"""
+    mmap=np.memmap('/tmp/tmp', dtype='float32', mode='w+', shape=demo.get_data().shape)
+    mmap_data_filtered[:,0,0]=array[:]
+    output=nb.MGHImage(mmap_data_filtered, demo.affine, demo.header)
+    nb.save(output, filename)
+
+measures=['.thickness_z_on_lh.sm10.mgh', '.asym.thickness_z.sm10.mgh',
     '.w-g.pct_z_on_lh.sm10.mgh','.asym.w-g.pct_z.sm10.mgh',
     '.pial.K_filtered_2_z_on_lh.sm20.mgh','.asym.pial.K_filtered_2_z.sm20.mgh',
         '.curv_on_lh.mgh','.sulc_on_lh.mgh',
@@ -35,4 +43,23 @@ measures=np.array(['.thickness_z_on_lh.sm10.mgh', '.asym.thickness_z.sm10.mgh',
     '.wm_FLAIR_0.5_z_on_lh.sm10.mgh','.wm_FLAIR_1_z_on_lh.sm10.mgh',
     '.asym.gm_FLAIR_0.75_z.sm10.mgh','.asym.gm_FLAIR_0.5_z.sm10.mgh',
     '.asym.gm_FLAIR_0.25_z.sm10.mgh','.asym.gm_FLAIR_0_z.sm10.mgh',
-'.asym.wm_FLAIR_0.5_z.sm10.mgh','.asym.wm_FLAIR_1_z.sm10.mgh'])
+'.asym.wm_FLAIR_0.5_z.sm10.mgh','.asym.wm_FLAIR_1_z.sm10.mgh']
+
+hemis=['lh','rh']
+
+
+demo=nb.load(subject_dir+subject_ids[0]+'/xhemi/surf/'+hemis[0]+measures[0])
+#cortex=nb.freesurfer.io.read_label(subject_dir+subject_ids[0]+'/xhemi/label/lh.cortrex')
+
+for h in hemis:
+    for m in measures:
+        control_data=np.zeros(len(subject_ids),len(demo.get_data()))
+        k=-1
+        for s in subject_ids:
+            k+=1
+            control_data[k]=import_mgh(subject_dir+s+'/xhemi/surf/'+h+m)
+        mean=np.mean(control_data,axis=1)
+        std=np.std(control_data,axis=1)
+        save_mgh(subject_dir+'template_control/'+h+'.mu'+m,mean,demo)
+        save_mgh(subject_dir+'template_control/'+h+'.std'+m,mean,demo)
+
--- scripts/meld_pipeline.sh
+++ scripts/meld_pipeline.sh
@@ -21,9 +21,10 @@ else
 #create the mu and std overlays for subsequent normalisation of controls and patients
     python create_control_subjects.py "$subjects_dir" "$subject_ids" 
 #normalise controls by mu and std
+    mkdir template_control
     python normalise_by_controls.py "$subjects_dir" "$subject_ids"
 #create training_data matrix
-    python create_training_data.py "$subjects_dir" "$subject_ids" meld_centre_prefix_controls
+    python create_training_data.py
 fi
 
 
@@ -46,27 +47,6 @@ else
 #move lesion labels across
     bash move_lesion_labels.sh "$subjects_dir" "$subject_ids"
 #create training_data matrix
-    python create_training_data.py "$subjects_dir" "$subject_ids" meld_centre_prefix_fcd
+    python create_training_data.py
 fi
-
-subject_ids="$subjects_dir"negs_ids.txt
-
-if [ ! -f "$subject_ids" ]; then
-    echo "No lesion negative ids found. skipping creating lesion_negatives tables"
-else
-    echo "FCD patients found, Running feature scripts..."
-#create basic features
-    bash Script1_sample_FLAIR_smooth_features.sh "$subjects_dir" "$subject_ids"
-#normalise intrasubject
-    python intrasubject_normalisation.py "$subjects_dir" "$subject_ids"
-#register to fsaverage xhemi
-    bash create_xhemi.sh "$subjects_dir" "$subject_ids"
-    bash Script_move_to_xhemi.sh "$subjects_dir" "$subject_ids"
-#normalise patients by mu and std
-    python normalise_by_controls.py "$subjects_dir" "$subject_ids"
-#create training_data matrix
-    python create_training_data.py "$subjects_dir" "$subject_ids" meld_centre_prefix_negs
-fi
-
-
 
